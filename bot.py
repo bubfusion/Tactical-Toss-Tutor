2d63af5090c4ef23465f1afbfd5769dcf374dd81
bubfusion <thebubfusion@gmail.com>
2024-04-06 19:32:46 -0400

    Fixed gen left issue on vertigo

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ vertigo = {
     "mid heaven": "https://imgur.com/i7ZzrTj \nhttps://imgur.com/kQhOFSa",
     "b headshot" : "https://imgur.com/8qKATMI \nhttps://imgur.com/433jTiK",
     "gen right" : "https://imgur.com/DG1UT2h \nhttps://imgur.com/CbvUiZV",
-    "gen left" : "https://imgur.com/ZcXtNfZ \nhttps://imgur.com/ZcXtNfZ",
+    "gen left" : "https://imgur.com/V4gB1Tp \nhttps://imgur.com/ZcXtNfZ",
     "a ramp" : "https://imgur.com/ogsC8Cp \nhttps://imgur.com/8qKATMI"
 }
 
